be07d0d64710bc56a47d46a2f53ed6946e227040
Zhiwei Zhong <33756305+zhwzhong@users.noreply.github.com>
2024-01-31 11:34:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ def main():
 
     train_data = get_loader(args, 'train')
 
-    if args.resume or args.test_only or args.pre_trained:
+    if args.resume or args.test_only:
         model_path = args.load_name if os.path.exists(args.load_name) else f"{cp_path}/{args.load_name}"
         try:
             checkpoint = torch.load(model_path, map_location='cuda:{}'.format(args.local_rank))
@@ -62,32 +62,7 @@ def main():
             print('===> File {} loaded'.format(model_path))
         evaluate(model, criterion, args.test_name, device=device, val_data=get_loader(args, args.test_name), args=args)
 
-    if not args.test_only:
-        for epoch in range(args.start_epoch, num_epochs):
-            if args.distributed:
-                train_data.sampler.set_epoch(epoch)
-            train_stats = train_one_epoch(model, criterion, train_data, optimizer, device, epoch, args)
-            log_stats = {**{f'TRAIN_{k}'.upper(): v for k, v in train_stats.items()}}
-
-            test_stats = evaluate(model, criterion, 'val', device=device, val_data=get_loader(args, 'val'), args=args)
-            log_stats.update({**{f'{k}'.upper(): v for k, v in test_stats.items()}})
-
-            test_stats = evaluate(model, criterion, 'test', device=device, val_data=get_loader(args, 'test'), args=args)
-            log_stats.update({**{f'{k}'.upper(): v for k, v in test_stats.items()}, 'EPOCH': epoch})
-
-            if args.local_rank == 0:
-                [writer.add_scalar(k.replace('_', '/'), v, epoch) for k, v in log_stats.items() if k != 'EPOCH']
-                with open("./logs/{}/{}/log.txt".format(args.dataset, args.file_name), 'a') as f:
-                    f.write(json.dumps(log_stats) + "\n")
-
-            utils.save_on_master({
-                'optimizer': optimizer.state_dict(),
-                'model': model_without_ddp.state_dict(),
-                'lr_scheduler': lr_scheduler.state_dict(),
-                'epoch': epoch,
-                'args': args,
-            }, '{}/model_{}.pth'.format(cp_path, str(epoch).zfill(6)))
-            lr_scheduler.step(epoch)
+   
 
 
 if __name__ == '__main__':
