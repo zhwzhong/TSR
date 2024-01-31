4516c71cd37d51d05a0293ccf007250d9918df17
Zhiwei Zhong <33756305+zhwzhong@users.noreply.github.com>
2024-01-31 12:27:27 -0300

    Update main.py

--- main.py
+++ main.py
@@ -40,26 +40,6 @@ def main():
     else:
         model = torch.nn.parallel.DataParallel(model, device_ids=list(range(args.num_gpus)))
     model_without_ddp = model.module
-
-    print('===> Parameter Number:', utils.get_parameter_number(model_without_ddp))
-
-    cp_path = f'./checkpoints/{args.dataset}/{args.file_name}'
-
-    train_data = get_loader(args, 'train')
-
-    if args.resume or args.test_only:
-        model_path = args.load_name if os.path.exists(args.load_name) else f"{cp_path}/{args.load_name}"
-        try:
-            checkpoint = torch.load(model_path, map_location='cuda:{}'.format(args.local_rank))
-            model_without_ddp.load_state_dict(checkpoint['model'])
-            if args.resume:
-                args.start_epoch = checkpoint['epoch'] + 1
-                lr_scheduler.step(args.start_epoch)
-                # optimizer.load_state_dict(checkpoint['optimizer'])
-        except FileNotFoundError:
-            print('===> File {} not exists'.format(model_path))
-        else:
-            print('===> File {} loaded'.format(model_path))
         evaluate(model, criterion, args.test_name, device=device, val_data=get_loader(args, args.test_name), args=args)
 
    
